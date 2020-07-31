15d8a936b95811f5d0db85e6e775c48622cce11c
lc <1033581609@qq.com>
2020-07-31 09:00:39 +0800

    修复点击icon图标不能跳转的bug

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,24 +4,26 @@ import { Link } from "react-router-dom";
 import Icon from './Icon';
 
 const NavWrapper = styled.nav`
-  line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
-  > ul {
+    line-height: 24px;
+    box-shadow: 0 0 3px rgba(0,0,0,0.25);
+    > ul {
     display: flex;
-    > li {
-      width: 33.3333%;
-      text-align: center;
-      padding: 16px;
-      display: flex;
-      padding: 4px 0;
-      flex-direction: column;
-      align-items: center;
-      .icon {
-          width: 24px;
-          height: 24px;
-      }
+        > li {
+            width: 33.3333%;
+            text-align: center;
+                > a{
+                padding: 16px;
+                padding: 4px 0;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                }
+            .icon {
+                width: 24px;
+                height: 24px;
+            }
+        }
     }
-  }
 `
 
 const Nav = () => {
@@ -29,16 +31,23 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
-                    <Icon fill="#d4237a" name="tag" />
-                    <Link to="/tags">标签页</Link>
+
+                    <Link to="/tags">
+                        <Icon fill="#d4237a" name="tag" />
+                        标签页
+                    </Link>
                 </li>
                 <li>
-                    <Icon fill="#da8d15" name="money" />
-                    <Link to="/money">记账页</Link>
+                    <Link to="/money">
+                        <Icon fill="#da8d15" name="money" />
+                        记账页
+                    </Link>
                 </li>
                 <li>
-                    <Icon fill="#1296db" name="chart" />
-                    <Link to="/statistics">统计页</Link>
+                    <Link to="/statistics">
+                        <Icon fill="#1296db" name="chart" />
+                        统计页
+                    </Link>
                 </li>
             </ul>
         </NavWrapper>
