5eb1051d91735eb1777756a158598d29ba3afb45
lc <1033581609@qq.com>
2020-07-31 10:38:12 +0800

    组件money UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,11 +1,62 @@
 import Layout from "components/Layout";
 import React from 'react'
+import { TagsSection } from './Money/TagsSection';
+import { CategorySection } from './Money/CategorySection';
+import { NotesSection } from './Money/NotesSection';
+import { NumberPadSection } from './Money/NumberPadSection';
+import styled from "styled-components";
+
+const MyLayout = styled(Layout)`
+    display:flex;
+    flex-direction: column;
+`
 
 function Money() {
     return (
-        <Layout>
-            <h2>账单</h2>
-        </Layout>
+        <MyLayout>
+            <TagsSection>
+                <ol>
+                    <li>衣</li>
+                    <li>食</li>
+                    <li>住</li>
+                    <li>行</li>
+                </ol>
+                <button>新增标签</button>
+            </TagsSection>
+            <NotesSection>
+                <label>
+                    <span>备注</span>
+                    <input type="text" placeholder="在这里添加备注" />
+                </label>
+            </NotesSection>
+            <CategorySection>
+                <ul>
+                    <li className="selected">支出</li>
+                    <li>收入</li>
+                </ul>
+            </CategorySection>
+            <NumberPadSection>
+                <div className="output">
+                    100
+                </div>
+                <div className="pad clearfix">
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>退格</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>清空</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button className="ok">OK</button>
+                    <button className="zero">0</button>
+                    <button className="dot">.</button>
+                </div>
+            </NumberPadSection>
+        </MyLayout>
     );
 }
 
---
+++ src/views/Money/TagsSection.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+const TagsSection = styled.section`
+    background: #FFFFFF; 
+    padding: 12px 16px;
+    flex-grow: 1; 
+    display:flex; 
+    flex-direction: column;
+    justify-content: flex-end; 
+    align-items: flex-start;
+    > ol { margin: 0 -12px;
+        > li{
+        background: #D9D9D9;
+        border-radius: 18px;
+        display:inline-block;
+        padding: 3px 18px; 
+        font-size: 14px;
+        margin: 8px 12px;
+        }
+    }
+    > button{
+        background:none; border: none;
+        padding: 2px 4px;
+        border-bottom: 1px solid gray;
+        color: #666;
+        margin-top: 8px;
+    }
+`;
+
+export { TagsSection };
